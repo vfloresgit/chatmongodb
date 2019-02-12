3c361a040cf62f5352fc05ce61a2f246dfce0cbc
victor hugo <victorfq18@gmail.com>
2019-02-11 23:35:06 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { ChatComponent } from './components/chat/chat.component';
+import { MenuComponent } from './directivas/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class PizzaPartyAppModule { }
